297fa3f51be6e33febc3b5c32479ecdb5279b456
lucindax <ahmedosmanazrk@gmail.com>
2018-02-12 18:44:11 +0200

    removing protocol from matching

--- lib/app.rb
+++ lib/app.rb
@@ -62,7 +62,7 @@ class App < Sinatra::Base
 
   helpers do
     def base_url
-      @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+      @base_url ||= "#{request.env['HTTP_HOST']}"
     end
   end
 
